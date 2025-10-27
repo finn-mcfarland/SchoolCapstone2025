9aff19dd487b6ebf84bb1b61260da10c138ab332
finn-mcfarland <148182810+finn-mcfarland@users.noreply.github.com>
2025-10-28 10:06:10 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch.nn.functional as F
 from fastapi.middleware.cors import CORSMiddleware
+import time
 
 app = FastAPI()
 
@@ -23,8 +24,19 @@ labels = ["Bullying", "Not Bullying"]
 
 @app.post("/classify")
 async def classify(req: CommentRequest):
+    start_time = time.time()  # start timer
+    
+    # model inference
     inputs = tokenizer([req.comment], return_tensors="pt", truncation=True, padding=True)
     outputs = model(**inputs)
     probs = F.softmax(outputs.logits, dim=-1)
     prediction = probs.argmax(dim=-1).item()
-    return {"comment": req.comment, "result": labels[prediction]}
+    
+    end_time = time.time()  # end timer
+    response_time_ms = (end_time - start_time) * 1000  # ms
+    
+    return {
+        "comment": req.comment,
+        "result": labels[prediction],
+        "response_time_ms": round(response_time_ms, 2)
+    }
